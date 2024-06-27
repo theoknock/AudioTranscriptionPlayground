db19f98a3eff2d5978dbac10d370329573ad404c
James Alan Bush <jamesalanbush1972@gmail.com>
2024-06-27 12:56:38 -0400

    Initial Commit

---
+++ AudioTranscriptionPlayground/AudioTranscriptionPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioTranscriptionPlaygroundApp.swift
+//  AudioTranscriptionPlayground
+//
+//  Created by Xcode Developer on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioTranscriptionPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
